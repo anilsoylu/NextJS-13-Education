3f81924db25464fc446a23bf7b077ca5c42ea537
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2022-12-27 12:57:37 +0300

    Blog Card

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import BlogCard from '../components/BlogCard';
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <h1 className="mb-5 text-3xl font-bold underline">Hello, Next.js!</h1>
       <BlogCard />
     </>
   );
--- components/BlogCard/index.jsx
+++ components/BlogCard/index.jsx
@@ -1,19 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import styles from './styles.module.css';
 
 function BlogCard() {
   return (
-    <Link href="/" className="flex flex-1 gap-x-5">
-      <div>
+    <Link
+      href="/"
+      className="flex items-center flex-1 gap-x-4 max-sm:flex-wrap"
+    >
+      <div className="relative h-24 w-24 flex-shrink-0 max-sm:mb-4  max-sm:h-[150px]  max-sm:w-[100%]">
         <Image
           src="https://picsum.photos/200/300"
           alt="blog image"
-          width={96}
-          height={96}
+          fill
+          priority={true}
+          className="object-cover border-r-8"
         />
       </div>
-      <div>// Blog Title // Blog Description</div>
+      <div>
+        <h3 className={`overflow-hidden text-lg font-bold ${styles.cardTitle}`}>
+          Blog Title
+        </h3>
+        <p
+          className={`card-paragraf mt-1 overflow-hidden ${styles.cardParaghraf}`}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+          repellat delectus quasi quae optio dignissimos eius mollitia totam
+          impedit. Ullam aut maxime nobis officia illo cum. Id suscipit deleniti
+          perspiciatis?
+        </p>
+      </div>
     </Link>
   );
 }
