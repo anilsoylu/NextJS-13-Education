83ffebef440650ad9d49ee3cc3a25476c66a8588
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2022-12-27 16:24:45 +0300

    loading page

---
+++ app/loading.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="spinnerWrapper">
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
