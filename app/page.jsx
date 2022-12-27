04311013cd4ddf3734d315e2653c9608c63ee6d4
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2022-12-27 14:56:24 +0300

    data fetching

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,20 @@
+import axios from 'axios';
 import BlogCard from '../components/BlogCard';
 
-export default function Home() {
+async function getPosts() {
+  const res = await axios.get('https://dummyjson.com/posts?limit=10');
+  return res.data;
+}
+
+export default async function Home() {
+  const { posts } = await getPosts();
+
   return (
     <>
       <h1 className="mb-5 text-3xl font-bold underline">Hello, Next.js!</h1>
-      <BlogCard />
+      {posts.map((post) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </>
   );
 }
--- components/BlogCard/index.jsx
+++ components/BlogCard/index.jsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 import React from 'react';
 import styles from './styles.module.css';
 
-function BlogCard() {
+function BlogCard({ post }) {
   return (
     <Link
       href="/"
-      className="flex items-center flex-1 gap-x-4 max-sm:flex-wrap"
+      className="flex items-center flex-1 mb-5 gap-x-4 max-sm:flex-wrap"
     >
-      <div className="relative h-24 w-24 flex-shrink-0 max-sm:mb-4  max-sm:h-[150px]  max-sm:w-[100%]">
+      <div className="max-sm:h-[150px relative h-24 w-24 flex-shrink-0 max-sm:mb-4 max-sm:w-[100%]">
         <Image
-          src="https://picsum.photos/200/300"
-          alt="blog image"
+          src={`https://picsum.photos/200/300?random=${post.id}`}
+          alt={post.title}
           fill
           priority={true}
           className="object-cover border-r-8"
@@ -20,15 +20,12 @@ function BlogCard() {
       </div>
       <div>
         <h3 className={`overflow-hidden text-lg font-bold ${styles.cardTitle}`}>
-          Blog Title
+          {post.title}
         </h3>
         <p
           className={`card-paragraf mt-1 overflow-hidden ${styles.cardParaghraf}`}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-          repellat delectus quasi quae optio dignissimos eius mollitia totam
-          impedit. Ullam aut maxime nobis officia illo cum. Id suscipit deleniti
-          perspiciatis?
+          {post.body}
         </p>
       </div>
     </Link>
